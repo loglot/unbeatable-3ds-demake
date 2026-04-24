399db004370dc80dfe044db23283d06548372dcb
loglot <prattlog3@gmail.com>
2026-04-24 10:06:34 -0500

    starting work on actual gameplay

--- source/games/unbeat.c
+++ source/games/unbeat.c
@@ -8,7 +8,7 @@
 #include <time.h>
 srand(time(NULL));
 
-// #include <3ds.h>
+
 void rgDrawBottom(){
     
     C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
@@ -19,10 +19,23 @@ void rgDrawBottom(){
     YACTRText("don't Press A!",100, 175,1,UI);
     C3D_FrameEnd(0);
 }
+void rgDrawTop(){
+    
+    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+    C2D_SceneBegin(top);
+    C2D_TargetClear(top, YAOL);
+    // C2D_DrawRectSolid(30, 30, 0, 260, 180, UI);
+    C2D_DrawCircleSolid(150, 70, 0, 15, white);
+    C2D_DrawCircleSolid(150, 140, 0, 15, white);
+    C2D_DrawCircleSolid(250, 70, 0, 15, white);
+    C2D_DrawCircleSolid(250, 140, 0, 15, white);
+    C2D_DrawCircleSolid(200, 140, 0, 20, white);
+    C3D_FrameEnd(0);
+}
 
 
 
-int rgTick() {
+void rgTick() {
     rgDrawBottom();
-
+    rgDrawTop();
 }
