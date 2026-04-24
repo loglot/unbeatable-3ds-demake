a73fd9a00207b852e365755b6a27709bc24abf55
loglot <prattlog3@gmail.com>
2026-04-24 18:45:36 -0500

    trying to do banner stuff

--- 3dsx-to-cia/makeCXITools.sh
+++ 3dsx-to-cia/makeCXITools.sh
@@ -6,7 +6,6 @@ git switch cxi-stuff
 make
 mv cxitool ../
 cd ..
-sudo rm -r 3dstools
 
 git clone https://github.com/3DSGuy/Project_CTR.git
 cd Project_CTR/makerom
@@ -14,4 +13,14 @@ make deps
 make
 mv bin/makerom ../../
 cd ../../
+
+git clone https://github.com/carstene1ns/3ds-bannertool.git
+cd 3ds-bannertool
+cmake -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build build
+mv build/bannertool ../
+cd ../ 
+
 sudo rm -r Project_CTR
+sudo rm -r 3dstools
+sudo rm -r 3ds-bannertool
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 ./3dsx-to-cia/cxitool unbeatable.3dsx unbeatable.cxi
-./3dsx-to-cia/makerom -v -f cia -o UNBEATABLE.cia -target t -i unbeatable.cxi:0:0 -ignoresign -icon unbeatable.smdh
+./3dsx-to-cia/bannertool makebanner -i banner2.png -o unbeatable.bnr -a empty.wav
+./3dsx-to-cia/makerom -v -f cia -o UNBEATABLE.cia -target t -i unbeatable.cxi:0:0 -ignoresign -icon unbeatable.smdh -banner unbeatable.bnr
