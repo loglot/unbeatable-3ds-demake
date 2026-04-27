68f5fdbb58ae3c6a45cdadb8f9374239b1938565
loglot <prattlog3@gmail.com>
2026-04-27 09:53:44 -0500

    the banner works now

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-./3dsx-to-cia/cxitool unbeatable.3dsx unbeatable.cxi
 ./3dsx-to-cia/bannertool makebanner -i banner2.png -o unbeatable.bnr -a empty.wav
-./3dsx-to-cia/makerom -v -f cia -o UNBEATABLE.cia -target t -i unbeatable.cxi:0:0 -ignoresign -icon unbeatable.smdh -banner unbeatable.bnr
+./3dsx-to-cia/cxitool unbeatable.3dsx unbeatable.cxi -b unbeatable.bnr
+./3dsx-to-cia/makerom -v -f cia -o UNBEATABLE.cia -target t -i unbeatable.cxi:0:0 -ignoresign
